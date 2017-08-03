18e988a8b3b66af652ce06b78ebd34726b06c6d2
dimon777227 <ma.ba1@rambler.ru>
2017-08-03 22:45:03 +0300

    function get structure data from nn implemented

--- NEAT.py
+++ NEAT.py
@@ -2,16 +2,38 @@ import numpy as np
 import random
 import math
 import pygame
+import copy
+
+CHANCE_MUTATION = 0.3
+CHANCE_ADD_CONNECTION = 0.2
+CHANCE_REMOVE_CONNECTION = 0.2
+CHANCE_ADD_NEURON = 0.1
+CHANCE_REMOVE_NEURON = 0.1
+CHANCE_CHANGE_WEIGHT = 0.5
+CHANCE_STRUCTURAL_CHANGE = 0.3
 
 
 class Connection():
-    def __init__(self, from_neuron, to_neuron):
-        self.weight = random.uniform(-2, 2)
+    CHANCE_RANDOMIZE_WEIGHT = 0.2
+
+    def __init__(self, from_neuron, to_neuron, weight=None):
+        if weight != None:
+            self.weight = weight
+        else:
+            self.weight = random.uniform(-2, 2)
         self.to_neuron = to_neuron
         self.from_neuron = from_neuron
 
         self.to_neuron.in_connections.append(self)
         self.from_neuron.out_connections.append(self)
+
+    def get_mutated_weight(self):
+        weight = self.weight
+        if random.random() < Connection.CHANCE_RANDOMIZE_WEIGHT:
+            weight = random.uniform(-2, 2)
+        else:
+            weight += random.uniform(-0.5, 0.5)
+        return weight
 
     def transfer(self):
         if self.from_neuron.out != None:
@@ -23,11 +45,18 @@ class Connection():
             return True
         return False
 
+    def __str__(self):
+        return str(round(self.weight, 4)) + ' ' + str(self.from_neuron.id) + ' ' + str(self.to_neuron.id)
+
 
 class Neuron():
     bias_out = 1
+    NUMBER = 0
 
     def __init__(self, input_neur=False, output_neur=False, bias=False):
+        self.id = Neuron.NUMBER
+        Neuron.NUMBER += 1
+
         self.is_input_neur = input_neur
         self.is_output_neur = output_neur
         self.bias = bias
@@ -56,6 +85,9 @@ class Neuron():
     def activate(self, x):
         a = math.tanh(x)
         return a
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Network():
@@ -90,8 +122,61 @@ class Network():
             prediction[i] = out_neur.out
         return prediction
 
+    def get_structure_data(self):
+        input_neurons = [Neuron(neu.is_output_neur, neu.is_output_neur, neu.bias) for neu in self.input_neurons]
+        h_neurons = [Neuron() for neu in self.h_neurons]
+        output_neurons = [Neuron(output_neur=True) for neu in self.output_neurons]
+
+        connections = [0 for conn in self.connections]
+
+        for i, conn in enumerate(self.connections):
+            if conn.from_neuron.is_input_neur:
+                if conn.to_neuron.is_output_neur:
+                    connections[i] = Connection(input_neurons[self.input_neurons.index(conn.from_neuron)],
+                                                output_neurons[self.output_neurons.index(conn.to_neuron)],
+                                                conn.weight)
+                else:
+                    connections[i] = Connection(input_neurons[self.input_neurons.index(conn.from_neuron)],
+                                                h_neurons[self.h_neurons.index(conn.to_neuron)],
+                                                conn.weight)
+            else:  # from neur is in hidden layer
+                if conn.to_neuron.is_output_neur:
+                    connections[i] = Connection(h_neurons[self.h_neurons.index(conn.from_neuron)],
+                                                output_neurons[self.output_neurons.index(conn.to_neuron)],
+                                                conn.weight)
+                else:  # to neur is in hidden layer
+                    connections[i] = Connection(h_neurons[self.h_neurons.index(conn.from_neuron)],
+                                                h_neurons[self.h_neurons.index(conn.to_neuron)],
+                                                conn.weight)
+
+        return input_neurons, h_neurons, output_neurons, connections
+
+    def create_offspring(self):
+        offspring = Network(len(self.input_neurons) - 1, len(self.output_neurons))
+
+        if random.random() < CHANCE_MUTATION:
+            while True:
+                if random.random() < CHANCE_CHANGE_WEIGHT:
+                    w = random.choice(self.connections).get_mutated_weight()
+
+                    break
+                if random.random() < CHANCE_STRUCTURAL_CHANGE:
+                    if random.random() < CHANCE_ADD_CONNECTION:
+                        pass
+                    if random.random() < CHANCE_REMOVE_CONNECTION:
+                        pass
+                    if random.random() < CHANCE_ADD_NEURON:
+                        pass
+                    if random.random() < CHANCE_REMOVE_NEURON:
+                        pass
+        else:
+            return copy.deepcopy(self)
+
 
 if __name__ == '__main__':
     n = Network(2, 3)
     res = n.predict((-0.2, 0.5))
     print(res)
+
+    data = n.get_structure_data()
+    print(list(map(str, data[0])), list(map(str, data[1])), list(map(str, data[2])), list(map(str, data[3])))
